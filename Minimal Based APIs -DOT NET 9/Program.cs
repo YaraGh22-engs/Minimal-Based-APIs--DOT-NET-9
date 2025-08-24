8f548ba3b96d956cbd92636d4b1601f46864a7c9
YaraGh <yaraesmaiel123@gmail.com>
2025-08-24 16:18:55 -0500

    HTTP Verb Support

--- Minimal Based APIs -DOT NET 9/Program.cs
+++ Minimal Based APIs -DOT NET 9/Program.cs
@@ -8,6 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 
+app.MapGet("/api/products", () => Results.Ok());
+
+app.MapPost("/api/products", () => Results.Ok());
+
+app.MapPut("/api/products/{id}", (Guid id) => Results.NoContent());
+
+app.MapPatch("/api/products/{id}", (Guid id) => Results.NoContent());
+
+app.MapDelete("/api/products/{id}", (Guid id) => Results.NoContent());
+
+//GENIRIC
+app.MapMethods("api/products", ["OPTIONS"], () => Results.NoContent());
+app.MapMethods("api/products", ["GET"], () => Results.Ok());
+app.MapMethods("api/products", ["GET","POST"], () => Results.NoContent());
 
 
 app.Run();
