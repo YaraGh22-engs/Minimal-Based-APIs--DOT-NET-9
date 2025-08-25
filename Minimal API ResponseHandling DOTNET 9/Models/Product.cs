8ca9e53539fe96f814b506f2122d108c63eb52c0
YaraGh <yaraesmaiel123@gmail.com>
2025-08-25 06:35:57 -0500

    Minimal API ResponseHandling DOTNET 9

---
+++ Minimal API ResponseHandling DOTNET 9/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace M02.MinimalEndpointAnatomy.Models;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+}
